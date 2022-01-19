991f71f49c184bf2ceaf6df1a0e0e72aff2a4299
BrayanSV18 <brayanvenegas24@gmail.com>
2022-01-19 17:45:33 -0600

    Parameters to receive instructions from the HTML file

--- dibujo.js
+++ dibujo.js
@@ -1,37 +1,16 @@
 var texto = document.getElementById("texto_lineas");
 var boton = document.getElementById("botoncito");
-boton.addEventListener("click", dibujoPorClick );
+//Posiciones
+var ii = document.getElementById("II");
+var id = document.getElementById("ID");
+var sd = document.getElementById("SD");
+var si = document.getElementById("SI");
+boton.addEventListener("click", dibujoPorClick);
 
 var d = document.getElementById("dibujito");
-var ancho = d.width;
 var lienzo = d.getContext("2d");
+var coulor = "#BD00FF";
+var ancho = d.width;
 
-function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
-{
-  lienzo.beginPath();
-  lienzo.strokeStyle = color;
-  lienzo.moveTo(xinicial, yinicial);
-  lienzo.lineTo(xfinal, yfinal);
-  lienzo.stroke();
-  lienzo.closePath();
-}
+var pix;
 
-function dibujoPorClick()
-{
-  var lineas = parseInt(texto.value);
-  var l = 0;
-  var yi, xf;
-  var colorcito = "#FAA";
-  var espacio = ancho / lineas;
-
-  for(l = 0; l < lineas; l++)
-  {
-    yi = espacio * l;
-    xf = espacio * (l + 1);
-    dibujarLinea(colorcito, 0, yi, xf, 300);
-    console.log("Linea " + l);
-  }
-
-  dibujarLinea(colorcito, 1,1,1,299);
-  dibujarLinea(colorcito, 1,299,299,299);
-}
